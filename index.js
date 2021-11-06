63925eb45d4e698ac252a38495e0d5e0a17b3599
danielgsantos15 <danir877@gmail.com>
2021-11-06 10:16:25 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -41,11 +41,6 @@ io.on('connection', (socket) => {
     socket.on('altera sanidade', (sanidade) => {
         io.emit('sanidade', sanidade)
     })
-    
-    socket.on('vida total', (vidaTotal) => {
-        io.emit('total', vidaTotal)
-        console.log(vidaTotal)
-    })
 
     console.log('a user connected');
 });
