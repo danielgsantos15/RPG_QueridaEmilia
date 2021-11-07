92b77a00bf3de23a95e788312dfac4981c151e3e
felipegoncalvesdsantos@gmail.com <felipe.santos@pontaltech.com.br>
2021-11-07 18:38:36 -0300

    praticando o socket

--- server.js
+++ server.js
@@ -4,31 +4,40 @@ const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
-const fs = require("fs");
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html")
 })
 
-app.get('/:name', (req, res) => {
+app.get('/:name', (req, res) => {    
     res.sendFile(__dirname + "/personagem.html");
 })
 
 io.on('connection', (socket) => {
-    socket.on('cria spersonagem', (personagem) => {
-        io.emit('definir personagem', personagem)
+    
+    socket.on('join', (room) => {
+        console.log(room)
+        socket.join(room)
+    })
+
+    socket.on('personagem privado', (room, personagem) => {
+        console.log(room, personagem)
+        socket.to(room).emit('definir personagem', personagem)
     });
 
-    socket.on('altera vida', (vida) => {
-        io.emit('vida', vida)
+    socket.on('altera vida', (room, vida) => {
+        socket.to(room).emit('vida', vida)
     })
     
-    socket.on('altera sanidade', (sanidade) => {
-        io.emit('sanidade', sanidade)
+    socket.on('altera sanidade', (room, sanidade) => {
+        socket.to(room).emit('sanidade', sanidade)
     })
 
-    console.log('a user connected');
+
+    console.log('a user connected', socket.id);
 });
+
+
 
 
 
